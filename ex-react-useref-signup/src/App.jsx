eb4042cdaf48353ab20b444e1a1fcb09e210e9ce
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-29 11:02:33 +0200

    [UPD] creazione della form e dei singoli stati per ogni input

--- ex-react-useref-signup/src/App.jsx
+++ ex-react-useref-signup/src/App.jsx
@@ -1,33 +1,57 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [name, setName] = useState('');
+
+  const [username, setUsername] = useState('');
+
+  const [password, setPassword] = useState('');
+
+  const [special, setSpecial] = useState('');
+
+  const [anni, setAnni] = useState('');
+
+  const [bio, setBio] = useState('');
+
+  const sendData = (e) => {
+    e.preventDefault()
+    console.log(`Riepilogo dati:
+      - Nome = ${name};
+      - Username = ${username}
+      - Password = ${password}
+      - Specializzazione = ${special}
+      - Anni di ex. = ${anni}
+      - Biografia = ${bio}
+      `)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='p-4 text-center text-white bg-primary'>EX - Web Developer Signup</header>
+      <main>
+        <div className="container mt-5">
+          <div className="row ">
+            <div className="col-lg-3 d-flex flex-column">
+              <form onSubmit={sendData}>
+                <input type="text" className=" mb-3 form-control" value={name} placeholder='Nome completo' onChange={(e) => setName(e.target.value)} />
+                <input type="text" className=" mb-3 form-control" placeholder=' Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" className=" mb-3 form-control" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <select className=" mb-3 form-control" value={special} onChange={(e) => setSpecial(e.target.value)}>
+                  <option >&#9660; Scegli la specializzazione </option>
+                  <option value="Full stack">Full Stack </option>
+                  <option value="Frontend">Frontend</option>
+                  <option value="Backend">Backend</option>
+                </select>
+                <input type="number" className=" mb-3 form-control" placeholder='Anni di esperienza' value={anni} onChange={(e) => setAnni(e.target.value)} />
+                <textarea className=" mb-3 form-control" placeholder='Breve descrizione' value={bio} onChange={(e) => setBio(e.target.value)}></textarea>
+                <button className='btn btn-primary'>Invia</button>
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </main>
     </>
   )
 }
